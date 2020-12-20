4ad9e23c1ff8e209e13750f9adf999cf211f90d9
zxdmac <zydrunemac@gmail.com>
2020-12-21 00:20:53 +0200

    directory renamed v2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'part3'`, () => {
+  it(`should have as title 'agmis-task'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('part3');
+    expect(app.title).toEqual('agmis-task');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('part3 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('agmis-task app is running!');
   });
 });
